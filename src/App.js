dc5c63e84b3b7a03bb40c9a08ec10eaaf5f53bc6
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-02-14 00:17:38 +0100

    equalized the flex ratio of all as presently available component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import TopNav from './Components/TopNav';
 import LeftNav from './Components/LeftNav';
 import TimeLine from './Components/TimeLine';
+import RightNav from './Components/RightNav';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <div className="app__body">
         <LeftNav />
         <TimeLine />
+        <RightNav />
       </div>
     </div>
   );
---
+++ src/Components/RightNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RightNav.css'
+
+const RightNav = () => {
+    return (
+        <div className='rightNav'>
+            
+        </div>
+    )
+}
+
+export default RightNav
